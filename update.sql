bd084c7458184a19b6e2c83ba2480e418518cc2c
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2014-11-30 13:09:01 +0100

    SQL

--- update.sql
+++ update.sql
@@ -11,11 +11,12 @@ alter table tag add column company_fk int8;
 update tag set company_fk = 9 where id < 1000;
 update tag set company_fk =31260 where id >= 1000;
 alter table tag ALTER COLUMN company_fk SET NOT NULL;
+delete from tag where id not in (select tag_id from product_tag);
 
 alter table feature_value add constraint FKBA64DEE8BC891E59 foreign key (feature_fk) references feature;
-
 alter table feature_value add constraint FKBA64DEE855ECAFB9 foreign key (product_fk) references product;
 
 alter table tag add constraint FK1BF9A602DCF9 foreign key (company_fk) references company;
+
 
 /* REASSIGN OWNED BY iper2010 to mogobiz; */
