e9f675ba69cf4ea66f9785df768d483bc14ee3b3
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-01-19 23:32:38 +0100

    SQL Update

--- update.sql
+++ update.sql
@@ -1,15 +1,3 @@
+DROP TABLE bo_returned_item;
 
-alter table category add column return_max_delay int8;
-update category set return_max_delay = 0;
-alter table category ALTER COLUMN return_max_delay SET NOT NULL;
-
-alter table product add column return_max_delay int8;
-update product set return_max_delay = 0;
-alter table product ALTER COLUMN return_max_delay SET NOT NULL;
-
-alter table xcatalog add column return_max_delay int8;
-update xcatalog set return_max_delay = 0;
-alter table xcatalog ALTER COLUMN return_max_delay SET NOT NULL;
-
-
-/* REASSIGN OWNED BY iper2010 to mogobiz; */
+ALTER TABLE b_o_delivery DROP destination;
