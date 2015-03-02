82387ab07a7740ad946e5b3b85cdcdc5745dbcbd
Stéphane Manciot <stephane.manciot@ebiznext.com>
2015-03-02 23:16:20 +0100

    update rx dependencies for groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -52,9 +52,8 @@ grails.project.dependency.resolution = {
 
         runtime 'com.typesafe.akka:akka-actor_2.11:2.3.9'
         runtime 'com.typesafe.akka:akka-stream-experimental_2.11:1.0-M3'
-        runtime 'io.reactivex:rxjava:1.0.7'
-        runtime 'io.reactivex:rxjava-reactive-streams:0.3.0'
-        runtime ('com.netflix.rxjava:rxjava-groovy:0.20.7') {excludes "groovy-all", "rxjava"}
+        runtime ('io.reactivex:rxjava-reactive-streams:0.3.0') {excludes "rxjava"}
+        runtime ('com.netflix.rxjava:rxjava-groovy:0.20.7') {excludes "groovy-all"}
 
         //oltu
         compile 'org.apache.oltu.oauth2:org.apache.oltu.oauth2.common:0.31'
