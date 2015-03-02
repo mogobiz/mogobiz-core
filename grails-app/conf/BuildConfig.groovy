dd0ec3d91835491a4a43e27195cdcda603102490
Stéphane Manciot <stephane.manciot@ebiznext.com>
2015-03-02 18:03:18 +0100

    update dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,17 +3,17 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+        // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+        //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+        // configure settings for the test-app JVM, uses the daemon by default
+        test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+        // configure settings for the run-app JVM
+        run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the run-war JVM
+        war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+        // configure settings for the Console UI JVM
+        console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
 grails.project.dependency.resolver = "maven" // or ivy
@@ -50,9 +50,11 @@ grails.project.dependency.resolution = {
 
         compile 'org.scala-lang:scala-library:2.11.2'
 
-        runtime 'com.typesafe.akka:akka-actor_2.11:2.3.3'
-        runtime 'com.typesafe.akka:akka-stream-experimental_2.11:0.4'
-        runtime ('com.netflix.rxjava:rxjava-groovy:0.16.1') {excludes "groovy-all"}
+        runtime 'com.typesafe.akka:akka-actor_2.11:2.3.9'
+        runtime 'com.typesafe.akka:akka-stream-experimental_2.11:1.0-M3'
+        runtime 'io.reactivex:rxjava:1.0.7'
+        runtime 'io.reactivex:rxjava-reactive-streams:0.3.0'
+        runtime ('com.netflix.rxjava:rxjava-groovy:0.20.7') {excludes "groovy-all", "rxjava"}
 
         //oltu
         compile 'org.apache.oltu.oauth2:org.apache.oltu.oauth2.common:0.31'
@@ -74,7 +76,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":release:3.0.1",
-              ":rest-client-builder:1.0.3") {
+                ":rest-client-builder:1.0.3") {
             export = false
         }
 
