24dfd54dab9ba69cd87d56400464da6951d884d2
Stéphane Manciot <stephane.manciot@ebiznext.com>
2015-05-01 11:19:17 +0200

    add tag lib for store permissions

---
+++ grails-app/taglib/com/mogobiz/store/StorePermissionTagLib.groovy
@@ -0,0 +1,80 @@
+package com.mogobiz.store
+
+import com.mogobiz.store.domain.Seller
+import com.mogobiz.utils.PermissionType
+import org.apache.shiro.SecurityUtils
+import org.apache.shiro.subject.Subject
+
+import static com.mogobiz.utils.ProfileUtils.*
+
+class StorePermissionTagLib {
+
+    static namespace = "store"
+
+    /**
+     * This tag only writes its body to the output if the current user
+     * has the given permission.
+     */
+    def hasPermission = { attrs, body ->
+        if (checkPermission(attrs, "hasPermission")) {
+            // Output the body text.
+            out << body()
+        }
+    }
+
+    /**
+     * This tag only writes its body to the output if the current user
+     * does not have the given permission.
+     */
+    def lacksPermission = { attrs, body ->
+        if (!checkPermission(attrs, "lacksPermission")) {
+            // Output the body text.
+            out << body()
+        }
+    }
+
+    /**
+     * Checks whether the current user has the permission specified in
+     * the given tag attributes. Returns <code>true</code> if the user
+     * has the permission, otherwise <code>false</code>.
+     */
+    private boolean checkPermission(attrs, tagname) {
+
+        def check = false
+
+        String permission = attrs["permission"]
+        if (!permission) {
+            throwTagError("Tag [$tagname] must have a [permission] one.")
+        }
+
+        def storePermission = PermissionType.findByKey(permission)
+        if(!storePermission){
+            throwTagError("No store permission found for [$permission].")
+        }
+
+        def id = attrs["id"] as Long
+        if(!id){
+            def seller = request.getAttribute("seller") as Seller ?: retrieveAuthenticatedSeller()
+            if(seller){
+                id = seller.company?.id
+            }
+        }
+
+        if(id){
+            check = SecurityUtils.subject.isPermitted(computeStorePermission(storePermission, id))
+        }
+
+        return check
+    }
+
+    private Seller retrieveAuthenticatedSeller(){
+        def seller = null
+        Subject subject = SecurityUtils.getSubject()
+        Object principal = subject?.getPrincipal()
+        if (principal != null && subject.isAuthenticated()) {
+            def login = principal.toString()
+            seller = Seller.findByLogin(login, [fetch: [company: 'join']])
+        }
+        return seller
+    }
+}
