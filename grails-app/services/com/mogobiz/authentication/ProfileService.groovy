d99dc2ed415aa2c49f82ce28efa3d6c1930f08a6
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-06-06 23:10:08 +0200

    Handle circular reference correctly in Profile/Authentication services  otherwise it can't work.

--- grails-app/services/com/mogobiz/authentication/ProfileService.groovy
+++ grails-app/services/com/mogobiz/authentication/ProfileService.groovy
@@ -22,10 +22,16 @@ import static com.mogobiz.utils.ProfileUtils.*
 @Transactional
 class ProfileService {
 
-    AuthenticationService authenticationService
+    // By making it protected we do not allow grails to inject the circular dependency
+    protected AuthenticationService authenticationService
 
     SanitizeUrlService sanitizeUrlService
 
+    def grailsApplication
+
+    def setCircularDependency() {
+        this.authenticationService = grailsApplication.mainContext.authenticationService
+    }
     /**
      *
      * @param p - profile
@@ -33,7 +39,7 @@ class ProfileService {
      * @param args - values to apply to the permission
      * @return ProfilePermission
      */
-    ProfilePermission getProfilePermission(Profile p, PermissionType type, String ... args){
+    ProfilePermission getProfilePermission(Profile p, PermissionType type, String... args) {
         DetachedCriteria<ProfilePermission> query = ProfilePermission.where {
             (target == computePermission(type, args)) && profile.id == p.id && permission.id == getWilcardPermission().id
         }
@@ -44,18 +50,15 @@ class ProfileService {
      *
      * @return wild card permission
      */
-    Permission getWilcardPermission(){
+    Permission getWilcardPermission() {
         Permission permission = Permission.findByTypeAndPossibleActions(WILDCARD_PERMISSION, ALL);
         if (!permission) {
-            permission = new Permission(type:WILDCARD_PERMISSION, possibleActions:ALL)
+            permission = new Permission(type: WILDCARD_PERMISSION, possibleActions: ALL)
             permission.validate()
-            if (permission.hasErrors())
-            {
+            if (permission.hasErrors()) {
                 permission.errors.allErrors.each { log.error(it) }
-            }
-            else
-            {
-                permission.save(flush:true)
+            } else {
+                permission.save(flush: true)
             }
         }
         permission
@@ -69,16 +72,15 @@ class ProfileService {
      * @param args - values to apply to the permission
      * @return added/removed ProfilePermission
      */
-    ProfilePermission saveProfilePermission(Profile p, boolean add, PermissionType type, String ... args){
+    ProfilePermission saveProfilePermission(Profile p, boolean add, PermissionType type, String... args) {
         ProfilePermission pp = type ? getProfilePermission(p, type, args) : null
-        if(!pp && type && add){
+        if (!pp && type && add) {
             pp = new ProfilePermission(target: computePermission(type, args), key: type.key, profile: p, permission: getWilcardPermission())
             pp.validate()
-            if(!pp.hasErrors()){
+            if (!pp.hasErrors()) {
                 pp.save(flush: true)
             }
-        }
-        else if(pp && !add){
+        } else if (pp && !add) {
             pp.delete(flush: true)
         }
         pp
@@ -91,10 +93,10 @@ class ProfileService {
      * @param name - profile name
      * @return created/upgraded Profile
      */
-    Profile applyProfile(Profile parent, Long idStore, String name = parent.name){
+    Profile applyProfile(Profile parent, Long idStore, String name = parent.name) {
         Company company = Company.load(idStore)
         Profile child = null
-        if(company && parent && !parent.company){
+        if (company && parent && !parent.company) {
             child = Profile.findByCompanyAndParent(company, parent) ?: new Profile(
                     code: sanitizeUrlService.sanitizeWithDashes(name),
                     parent: parent,
@@ -102,8 +104,8 @@ class ProfileService {
             )
             child.name = name
             child.validate()
-            if(!child.hasErrors()){
-                child.save(flush:true)
+            if (!child.hasErrors()) {
+                child.save(flush: true)
                 upgradeProfile(child)
             }
         }
@@ -115,21 +117,21 @@ class ProfileService {
      * @param idProfile - id profile
      * @return Profile
      */
-    Profile unbindProfile(Profile child){
-        if(child && child.parent){
+    Profile unbindProfile(Profile child) {
+        if (child && child.parent) {
             child.parent = null
             child.save(flush: true)
         }
         child
     }
 
-    void removeProfile(Profile profile){
-        User.executeQuery('select u from User u left join u.profiles profile where profile=:profile', [profile:profile]).each {user ->
+    void removeProfile(Profile profile) {
+        User.executeQuery('select u from User u left join u.profiles profile where profile=:profile', [profile: profile]).each { user ->
             user.removeFromProfiles(profile)
             user.save(flush: true)
         }
-        Profile.findAllByParent(profile).each{child ->
-            User.executeQuery('select u from User u left join u.profiles profile where profile=:profile', [profile:child]).each {user ->
+        Profile.findAllByParent(profile).each { child ->
+            User.executeQuery('select u from User u left join u.profiles profile where profile=:profile', [profile: child]).each { user ->
                 user.removeFromProfiles(profile)
                 user.save(flush: true)
             }
@@ -144,7 +146,7 @@ class ProfileService {
      * @param parent - the parent profile from whom the permissions will be copied
      */
     void upgradeProfile(Profile child, Profile parent = child?.parent) {
-        if((parent && !parent.company) || child.company?.id == parent.company?.id){
+        if ((parent && !parent.company) || child.company?.id == parent.company?.id) {
             def wildCard = getWilcardPermission()
             def oldPermissions = ProfilePermission.findAllByProfile(child)
             oldPermissions.each { it.delete(flush: true) }
@@ -158,7 +160,7 @@ class ProfileService {
                         target: MessageFormat.format(parentPermission.target, child.company?.id as String)
                 )
                 pp.validate()
-                if(!pp.hasErrors()){
+                if (!pp.hasErrors()) {
                     pp.save(flush: true)
                     log.debug("added ${pp.key} to ${child.company?.id ?: '*'}:${child.name}")
                 }
@@ -170,9 +172,9 @@ class ProfileService {
      * upgrade all profiles which are linked to the profile whose id has been passed as parameter
      * @param parent - parent profile
      */
-    void upgradeChildProfiles(Profile parent){
-        if(parent && !parent.company){
-            Profile.findAllByParent(parent).each {child ->
+    void upgradeChildProfiles(Profile parent) {
+        if (parent && !parent.company) {
+            Profile.findAllByParent(parent).each { child ->
                 upgradeProfile(child)
             }
         }
@@ -183,8 +185,8 @@ class ProfileService {
      * @param user - user
      * @param profile - profile
      */
-    void addUserProfile(User user, Profile profile){
-        if(profile && user && profile.company.id == user.company.id){
+    void addUserProfile(User user, Profile profile) {
+        if (profile && user && profile.company.id == user.company.id) {
             user.addToProfiles(profile)
             user.save(flush: true)
         }
@@ -195,8 +197,8 @@ class ProfileService {
      * @param user - user
      * @param profile - profile
      */
-    void removeUserProfile(User user, Profile profile ){
-        if(profile && user && profile in user.profiles){
+    void removeUserProfile(User user, Profile profile) {
+        if (profile && user && profile in user.profiles) {
             user.removeFromProfiles(profile)
             user.save(flush: true)
         }
@@ -221,72 +223,72 @@ class ProfileService {
         saveUserPermission(user, wasAdmin || isAdmin(user), PermissionType.ADMIN_COMPANY, user.company.id as String)
     }
 
-    boolean isSeller(User user){
-        if(!user){
+    boolean isSeller(User user) {
+        if (!user) {
             return false
         }
-        user.profiles.any {profile ->
-            profile.permissions.any {permission ->
-                retrievePermissionFrom(permission.target)?.key in PermissionType.seller().collect {it.key}
+        user.profiles.any { profile ->
+            profile.permissions.any { permission ->
+                retrievePermissionFrom(permission.target)?.key in PermissionType.seller().collect { it.key }
             }
         }
     }
 
-    boolean isValidator(User user){
-        if(!user){
+    boolean isValidator(User user) {
+        if (!user) {
             return false
         }
-        user.profiles.any {profile ->
-            profile.permissions.any {permission ->
-                retrievePermissionFrom(permission.target)?.key in PermissionType.validator().collect {it.key}
+        user.profiles.any { profile ->
+            profile.permissions.any { permission ->
+                retrievePermissionFrom(permission.target)?.key in PermissionType.validator().collect { it.key }
             }
         }
     }
 
-    boolean isAdmin(User user){
-        if(!user){
+    boolean isAdmin(User user) {
+        if (!user) {
             return false
         }
-        user.profiles.any {profile ->
-            profile.permissions.any {permission ->
-                retrievePermissionFrom(permission.target)?.key in PermissionType.admin().collect {it.key}
+        user.profiles.any { profile ->
+            profile.permissions.any { permission ->
+                retrievePermissionFrom(permission.target)?.key in PermissionType.admin().collect { it.key }
             }
         }
     }
 
-    boolean containsUserPermission(User u, PermissionType type, String ... args){
+    boolean containsUserPermission(User u, PermissionType type, String... args) {
         getUserPermission(u, type, args) != null
     }
 
-    UserPermission getUserPermission(User u, PermissionType type, String ... args){
+    UserPermission getUserPermission(User u, PermissionType type, String... args) {
         DetachedCriteria<UserPermission> query = UserPermission.where {
             (target == computePermission(type, args)) && user.id == u.id && permission.id == getWilcardPermission().id
         }
         query.get()
     }
 
-    Collection<User> getUsersGrantedPermission(PermissionType type, String ... args){
+    Collection<User> getUsersGrantedPermission(PermissionType type, String... args) {
         def query = UserPermission.where {
             (target == computePermission(type, args)) && permission.id == getWilcardPermission().id
         }
-        def users = query.list([fetch: [user: 'join']]).collect {userPermission ->
+        def users = query.list([fetch: [user: 'join']]).collect { userPermission ->
             userPermission.user
         }
         users
     }
 
-    UserPermission saveUserPermission(User user, boolean add, PermissionType type, String ... args){
+    UserPermission saveUserPermission(User user, boolean add, PermissionType type, String... args) {
         String target = type ? computePermission(type, args) : null
-        switch(type){
+        switch (type) {
             case [PermissionType.UPDATE_STORE_CATEGORY_WITHIN_CATALOG]:
-                if(args.size() == 3){
+                if (args.size() == 3) {
                     def idCategory = args[2]
-                    if(idCategory != ALL){
+                    if (idCategory != ALL) {
                         def parent = Category.load(idCategory as Long)
-                        if(parent){
+                        if (parent) {
                             def categories = Category.findAllByParent(parent)
-                            if(!categories.isEmpty()){
-                                categories.each {category ->
+                            if (!categories.isEmpty()) {
+                                categories.each { category ->
                                     List<String> l = []
                                     l.addAll(args.take(2))
                                     l << (category.id as String)
@@ -302,43 +304,41 @@ class ProfileService {
         }
         def wildCardPermission = getWilcardPermission()
         def userPermission = type && user ? getUserPermission(user, type, args) : null
-        if(!userPermission && user && target && add){
+        if (!userPermission && user && target && add) {
             userPermission = new UserPermission(
                     permission: wildCardPermission,
                     target: target,
                     key: type.key,
-                    user: user).save(flush:true)
-        }
-        else if(userPermission && !add){
+                    user: user).save(flush: true)
+        } else if (userPermission && !add) {
             user.removeFromPermissions(userPermission)
             user.save(flush: true)
         }
         userPermission
     }
 
-    RolePermission getRolePermission(Role r, PermissionType type, String ... args){
+    RolePermission getRolePermission(Role r, PermissionType type, String... args) {
         DetachedCriteria<RolePermission> query = RolePermission.where {
             (target == computePermission(type, args)) && role.id == r.id && permission.id == getWilcardPermission().id
         }
         query.get()
     }
 
-    RolePermission saveRolePermission(Role role, boolean add, PermissionType type, String ... args){
+    RolePermission saveRolePermission(Role role, boolean add, PermissionType type, String... args) {
         String target = type ? computePermission(type, args) : null
         def wildCardPermission = getWilcardPermission()
         def rolePermission = type && role ? getRolePermission(role, type, args) : null
-        if(!rolePermission && role && target && add){
+        if (!rolePermission && role && target && add) {
             rolePermission = new RolePermission(
                     permission: wildCardPermission,
                     target: target,
                     key: type.key,
                     role: role)
             rolePermission.validate()
-            if(!rolePermission.hasErrors()){
+            if (!rolePermission.hasErrors()) {
                 rolePermission.save(flush: true)
             }
-        }
-        else if(rolePermission && !add){
+        } else if (rolePermission && !add) {
             role.removeFromPermissions(rolePermission)
             role.save(flush: true)
         }
@@ -353,10 +353,10 @@ class ProfileService {
      * @param name - the profile name
      * @return id of the copied profile
      */
-    Profile copyProfile(Profile parent, Long idStore, String name = parent.name){
+    Profile copyProfile(Profile parent, Long idStore, String name = parent.name) {
         Company company = Company.load(idStore)
         Profile child = null
-        if(company && parent && (!parent.company || parent.company.id == idStore)){
+        if (company && parent && (!parent.company || parent.company.id == idStore)) {
             def code = sanitizeUrlService.sanitizeWithDashes(name)
             child = Profile.findByCompanyAndCode(company, code) ?: new Profile(
                     code: code,
@@ -364,7 +364,7 @@ class ProfileService {
             )
             child.name = name
             child.validate()
-            if(!child.hasErrors()){
+            if (!child.hasErrors()) {
                 child.save(flush: true)
                 upgradeProfile(child, parent)
             }
