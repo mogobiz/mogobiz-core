660b60d1cef4e3637c86455b177b241eb7d08fbc
Stephane Manciot <stephane.manciot@ebiznext.com>
2016-08-02 10:22:42 +0300

    add EsSync

---
+++ grails-app/domain/com/mogobiz/store/domain/EsSync.groovy
@@ -0,0 +1,84 @@
+package com.mogobiz.store.domain
+
+import grails.persistence.Entity
+import groovy.transform.EqualsAndHashCode
+
+/**
+ *
+ */
+@Entity
+@EqualsAndHashCode(includes="id")
+class EsSync
+    implements java.io.Serializable
+{
+    def esSyncValidation
+    def esSyncRender
+
+    java.util.Date dateCreated
+    java.util.Date lastUpdated
+
+    String uuid = java.util.UUID.randomUUID().toString()
+
+    /**
+     *
+     */
+    java.lang.String report
+    /**
+     *
+     */
+    Boolean success
+    /**
+     *
+     */
+    java.util.Date timestamp
+    /**
+     *
+     */
+    com.mogobiz.store.domain.EsEnv esEnv
+    /**
+     *
+     */
+    com.mogobiz.store.domain.Company company
+    /**
+     *
+     */
+    com.mogobiz.store.domain.Catalog catalog
+
+    static transients = [ 'esSyncValidation', 'esSyncRender' ]
+
+    static mapping = {
+
+        autoTimestamp true
+
+        uuid column: "uuid", insertable: true, updateable: false, lazy: false, cache: false
+
+        cache usage: 'read-write'
+
+        tablePerHierarchy false
+
+        table 'es_sync'
+
+        version false
+
+        id name: 'id', column: 'id', generator: 'native'
+        report column:"report",insertable:true,updateable:true,lazy:false,type:"text",cache:false
+        success column:"success",insertable:true,updateable:true,lazy:false,cache:false
+        timestamp column:"timestamp",insertable:true,updateable:true,lazy:false,cache:false
+
+
+        esEnv column:"es_env_fk",insertable:true,updateable:true,lazy:true,cache:'read-write'
+        company column:"company_fk",insertable:true,updateable:true,lazy:true,cache:'read-write'
+        catalog column:"catalog_fk",insertable:true,updateable:true,lazy:true,cache:'read-write'
+    }
+
+    static hasMany = [categories: Category, products: Product]
+
+    static constraints = {
+        report ( nullable:true, unique:false)
+        success ( nullable:true, unique:false)
+        timestamp ( nullable:true, unique:false)
+        company ( nullable:false, unique:false)
+        esEnv ( nullable:false, unique:false)
+        catalog ( nullable:true, unique:false)
+    }
+}
